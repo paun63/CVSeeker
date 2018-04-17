3ef973b7eb27d64a7a160928fe8d78908031c272
nsolunac <35047525+nsolunac@users.noreply.github.com>
2018-04-17 15:31:26 +0200

    Add files via upload

---
+++ CVSeekerFront/src/java/jsf/mb/MBPrijava.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsf.mb;
+
+import java.io.Serializable;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Paun
+ */
+@Named(value = "mbPrijava")
+@SessionScoped
+public class MBPrijava implements Serializable {
+}
